8827fc89c7a44b5dd10f8345391130a71d3aa51d
Artem Tarasov <lomereiter@gmail.com>
2012-06-20 14:04:35 +0400

    bam-fetch - make regions optional

--- CLItools/bam-fetch/bam_fetch.d
+++ CLItools/bam-fetch/bam_fetch.d
@@ -10,7 +10,7 @@ import std.getopt;
 import std.algorithm;
 
 void printUsage(string program) {
-    writeln("Usage: " ~ program ~ " [options] <input.bam> region1 [...]");
+    writeln("Usage: " ~ program ~ " [options] <input.bam> [region1 [...]]");
     writeln();
     writeln("Options: -q, --quality-threshold=THRESHOLD");
     writeln("                    skip reads with mapping quality < THRESHOLD");
@@ -28,29 +28,46 @@ int main(string[] args) {
                "quality-threshold|q", &quality_threshold,
                "read-group|r",        &read_group);
         
-        if (args.length < 3) {
+        if (args.length < 2) {
             printUsage(args[0]);
             return 0;
         }
 
         auto bam = BamFile(args[1]);
-        auto regions = map!parseRegion(args[2 .. $]);
 
-		foreach (ref r; regions) {
-			foreach (read; bam[r.reference][r.beg .. r.end]) {
-				if (quality_threshold != -1 && read.mapping_quality < quality_threshold) {
-					continue;
-				}
-				if (read_group !is null) {
-					auto rg = read["RG"];
-					if (!rg.is_string || (to!string(rg) != read_group)) {
-						continue;
-					}
-				}
-				serialize(read, bam.reference_sequences, stdout);
-				putcharacter(stdout, '\n');
-			}
-		}
+        bool satisfiesPredicate(ref Alignment read) {
+            if (quality_threshold != -1 && read.mapping_quality < quality_threshold) {
+                return false;
+            }
+            if (read_group !is null) {
+                auto rg = read["RG"];
+                if (!rg.is_string || (to!string(rg) != read_group)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        if (args.length == 2) {
+            foreach (read; bam.alignments) {
+                if (satisfiesPredicate(read)) {
+                    serialize(read, bam.reference_sequences, stdout);
+                    putcharacter(stdout, '\n');
+                }
+            }
+        } else {
+            auto regions = map!parseRegion(args[2 .. $]);
+
+            foreach (ref r; regions) {
+                foreach (read; bam[r.reference][r.beg .. r.end]) {
+                    if (satisfiesPredicate(read)) {
+                        serialize(read, bam.reference_sequences, stdout);
+                        putcharacter(stdout, '\n');
+                    }
+                }
+            }
+        }
+
     } catch (Exception e) {
         writeln("bam-fetch: ", e.msg);
         return 1;
