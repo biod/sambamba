38e0e80024dcfad4ebd2b0f3ed35cb5667fef666
lomereiter <lomereiter@gmail.com>
2012-10-12 00:11:30 +0400

    ...

--- snpcallers/bayesian.d
+++ snpcallers/bayesian.d
@@ -6,15 +6,36 @@ import std.algorithm;
 import std.typecons;
 import std.range;
 import std.math;
+import std.array;
+import std.mathspecial;
 import std.conv;
+import utils.algo;
 
 private {
 
-    immutable float[] table;
+    immutable HETEROZYGOTE_PROB = 0.001; // FIXME: there should be an option to
+                                         //        use per-site prior allele frequencies
+
+    immutable float LOG_BAR_R;
+
+    float[] lg_gamma_table;
 
     static this() {
-        table = cast(immutable)array(take(recurrence!"a[n-1] * 0.85f"(1.0f), 512));
+        LOG_BAR_R = log10(2 * HETEROZYGOTE_PROB / (1 - HETEROZYGOTE_PROB));
+
+        lg_gamma_table.length = 8192;
+        for (auto i = 1; i < lg_gamma_table.length; i++) {
+            lg_gamma_table[i] = logGamma(i) / LN10;
+        }
     }
+
+    auto ctfeArray(R)(R range) {
+        ElementType!R[] r;
+        foreach (e; range) r ~= e;
+        return r;
+    }
+
+    immutable float[] table = ctfeArray(take(recurrence!"a[n-1] * 0.85f"(1.0f), 512));
 
     float f(size_t k) {
         return k >= table.length ? 0.0f : table[k];
@@ -28,6 +49,7 @@ private {
         float minus_log10_base_error = cast(float)base_error / 10.0;
         return -minus_log10_base_error;
     }
+
 
     // Computes probability that bases corresponding to error_rates_wrong
     // are indeed wrong, and bases that correspond to error_rates_correct
@@ -66,8 +88,25 @@ private {
     }
 
     float coeff(size_t n, size_t k, float log10_base_error) {
-        return 1.0f; // TODO: use precomputed table
+        return 0.0f; // TODO: use precomputed table
     }
+
+    float lgGamma(size_t n) {
+        if (n >= lg_gamma_table.length) {
+            auto old_len = lg_gamma_table.length;
+            lg_gamma_table.length = n + 1;
+            for (auto i = old_len; i <= n; i++)
+                lg_gamma_table[i] = logGamma(i) / LN10;
+        }
+        return lg_gamma_table[n];
+    }
+}
+
+// Get normalized read quality.
+// The idea to use minimum of mapping quality and base quality is mentioned
+// in MAQ paper, so let's use it too here.
+float getQuality(R)(R read) {
+    return normalize(min(read.mapping_quality, read.current_base_quality));
 }
 
 struct BayesianSnpInfo {
@@ -83,13 +122,13 @@ Nullable!BayesianSnpInfo snpInfo(C)(ref C column, ref BayesianCallerSettings set
         return result;
     }
 
+    auto reads = filter!(read => read.current_base != '-')(column.reads);
+
     uint[char] base_counts;
 
-    foreach (read; column.reads) {
+    foreach (read; reads) {
         auto base = read.current_base;
-        if (base != '-') {
-            base_counts[base] += 1;
-        }
+        base_counts[base] += 1;
     }
 
     if (base_counts.length == 0) {
@@ -104,7 +143,7 @@ Nullable!BayesianSnpInfo snpInfo(C)(ref C column, ref BayesianCallerSettings set
             return result;
         }
 
-        auto error_rates = map!(r => normalize(r.current_base_quality))(column.reads);
+        auto error_rates = map!getQuality(reads);
 
         auto log10_site_error = logErrorProb(error_rates, cast(float[])[]);
 
@@ -119,19 +158,55 @@ Nullable!BayesianSnpInfo snpInfo(C)(ref C column, ref BayesianCallerSettings set
         result = info;
         return result;
     }
-    /* TODO
+
     if (base_counts.length >= 2) {
         // select two more frequent bases
-        Base[] alleles = base_counts.values;
-        partialSort!((a, b) => base_counts[a] > base_counts[b])(alleles, 2);
+        auto alleles = array(base_counts.keys);
+
+        bool cmpFunc(dchar a, dchar b) { return base_counts[cast(char)a] > base_counts[cast(char)b]; }
+        sort!cmpFunc(alleles); /* FIXME: get rid of AAs */
 
         Genotype[3] genotypes;
         float[3] log_probs;
-        genotypes[0] = Genotype(alleles[0]);
-        genotypes[1] = Genotype(alleles[0], alleles[1]);
-        genotypes[2] = Genotype(alleles[1]);
+        genotypes[0] = Genotype(Base(cast(char)alleles[0]));
+        genotypes[1] = Genotype(Base(cast(char)alleles[0]), Base(cast(char)alleles[1]));
+        genotypes[2] = Genotype(Base(cast(char)alleles[1]));
 
-    }*/
+        auto first_allele_reads = filter!(read => read.current_base == alleles[0])(reads);
+        auto second_allele_reads = filter!(read => read.current_base == alleles[1])(reads);
+        auto error_rates_fst = map!getQuality(first_allele_reads);
+        auto error_rates_snd = map!getQuality(second_allele_reads);
+         
+        log_probs[0] = logErrorProb(error_rates_snd, error_rates_fst);
+        log_probs[2] = logErrorProb(error_rates_fst, error_rates_snd);
+
+        auto k = walkLength(error_rates_fst);
+        auto n = k + walkLength(error_rates_snd);
+        log_probs[1] = LOG_BAR_R - n * LOG2 + 
+                       (lgGamma(n + 1) - lgGamma(k + 1) - lgGamma(n - k + 1));
+
+
+        auto best_index = argmax!(j => log_probs[j])(iota(3));
+       
+        BayesianSnpInfo info;
+        auto g = genotypes[best_index];
+
+        if (g.bases[0] == g.bases[1] && g.bases[0] == column.reference_base) {
+            return result;
+        }
+
+        info.estimated_genotype = g;
+
+        auto second_best_index = argmax!(j => log_probs[j])(filter!(j => j != best_index)(iota(3)));
+
+        info.quality = log_probs[best_index] - log_probs[second_best_index];
+        if (info.quality < settings.minimum_quality) {
+            return result;
+        }
+
+        result = info;
+        return result;
+    }
 
     return result;
 }
@@ -165,6 +240,14 @@ struct Genotype {
 
     /// Alleles (equal to each other in case of homozygosity)
     Base[2] bases;
+
+    string toString() const {
+        if (bases[0] == bases[1]) {
+            return to!string(bases[0]);
+        } else {
+            return to!string(bases[0]) ~ "," ~ to!string(bases[1]);
+        }
+    }
 }
 
 /// Column augmented with estimated genotype and its quality
