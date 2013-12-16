d052998ec62f7b4ffde2bfd3ca712b0a0f072ceb
Artem Tarasov <lomereiter@gmail.com>
2013-12-16 19:37:00 +0100

    added a test

--- .run_tests.sh
+++ .run_tests.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
-rdmd --compiler=ldmd2 -unittest -IBioD BioD/test/unittests.d
-
 ./build/sambamba sort -t2 -n BioD/test/data/ex1_header.bam -o ex1_header.nsorted.bam
+./build/sambamba view -t2 ex1_header.nsorted.bam | cut -f1 | LC_COLLATE=C sort -c
 ./build/sambamba sort -t2 ex1_header.nsorted.bam -o ex1_header.sorted.bam
 ./build/sambamba index -t2 ex1_header.sorted.bam
 ./build/sambamba slice ex1_header.sorted.bam chr1 -o /dev/null
-./build/sambamba view -c ex1_header.sorted.bam chr2 > /dev/null
-./build/sambamba view -c ex1_header.sorted.bam '*' > /dev/null
-rm ex1_header*bam
+./build/sambamba view -c ex1_header.sorted.bam chr2
+./build/sambamba view -c ex1_header.sorted.bam chr1
+./build/sambamba view -c ex1_header.sorted.bam '*' 
