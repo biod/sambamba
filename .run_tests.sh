65bef220688594b42401d5376f5da92aaa6ccc67
lomereiter <lomereiter@gmail.com>
2013-12-18 22:17:54 +0400

    tar on travis doesn't support --overwrite option

--- .run_tests.sh
+++ .run_tests.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-curl -L "https://dl.dropboxusercontent.com/u/7916095/shunit2-2.0.3.tgz" | tar zx --overwrite
+# download shunit2 in order to run tests:
+# curl -L "https://dl.dropboxusercontent.com/u/7916095/shunit2-2.0.3.tgz" | tar zx --overwrite
+
 ./.test_suite.sh | tee /dev/stderr | grep -q 'success rate: 100%'
