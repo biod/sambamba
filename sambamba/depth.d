b38e47fe680938d921672e60fc9296680c14cdff
Artem Tarasov <lomereiter@gmail.com>
2014-08-06 17:28:30 +0200

    fix

--- sambamba/depth.d
+++ sambamba/depth.d
@@ -17,7 +17,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
-module sambamba.view;
+module sambamba.depth;
 
 import bio.bam.reader;
 import bio.bam.pileup;
