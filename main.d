85ffe073dec7718393764f4b1deff4a0041b9fb2
Artem Tarasov <lomereiter@gmail.com>
2014-12-11 22:48:29 +0300

    v0.5.0; remove old files

--- main.d
+++ main.d
@@ -32,7 +32,7 @@ import sambamba.utils.common.ldc_gc_workaround;
 import std.stdio;
 
 void printUsage() {
-    stderr.writeln("sambamba v0.5.0-dev");
+    stderr.writeln("sambamba v0.5.0");
     stderr.writeln();
     stderr.writeln("Usage: sambamba [command] [args...]");
     stderr.writeln();
