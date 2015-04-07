7f76f498271c9cbcb149972bd067ec8feb9fba93
Artem Tarasov <lomereiter@gmail.com>
2015-04-07 11:12:30 +0300

    bump version

--- main.d
+++ main.d
@@ -32,7 +32,7 @@ import sambamba.utils.common.ldc_gc_workaround;
 import std.stdio;
 
 void printUsage() {
-    stderr.writeln("sambamba v0.5.1");
+    stderr.writeln("sambamba v0.5.2");
     stderr.writeln();
     stderr.writeln("Usage: sambamba [command] [args...]");
     stderr.writeln();
