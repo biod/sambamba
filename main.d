e3ff8e6a8b9fcd76defa6b04b6bbe3fc15ecb08c
Artem Tarasov <lomereiter@gmail.com>
2015-10-01 20:45:13 +0200

    bump version

--- main.d
+++ main.d
@@ -36,7 +36,7 @@ import utils.lz4;
 import std.stdio;
 
 void printUsage() {
-    stderr.writeln("sambamba v0.5.8");
+    stderr.writeln("sambamba v0.5.9");
     stderr.writeln();
     stderr.writeln("Usage: sambamba [command] [args...]");
     stderr.writeln();
