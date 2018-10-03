7c0e35d2f842b537c147300a374f7e2185dc7db9
Pjotr Prins <pjotr.public01@thebird.nl>
2018-10-03 11:54:28 +0000

    Travis: trying to fix tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,10 @@
 # download shunit2 in order to run tests:
 # curl -L "https://dl.dropboxusercontent.com/u/7916095/shunit2-2.0.3.tgz" | tar zx --overwrite
 
+echo "Running tests..."
+./test/test_suite.sh
+echo $?
+echo "Run2..."
 ./test/test_suite.sh 2>&1 | tr '\r' '\n' > test.log
 cat test.log
 cat test.log | grep -q 'success rate: 100%'
