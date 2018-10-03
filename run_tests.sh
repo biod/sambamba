b69b0e351ce3284cc3f9500b640d55e57c11e74f
Pjotr Prins <pjotr.public01@thebird.nl>
2018-10-03 12:03:12 +0000

    Travis: fixing tests on Linux

--- run_tests.sh
+++ run_tests.sh
@@ -4,9 +4,6 @@
 # curl -L "https://dl.dropboxusercontent.com/u/7916095/shunit2-2.0.3.tgz" | tar zx --overwrite
 
 echo "Running tests..."
-./test/test_suite.sh
-echo $?
-echo "Run2..."
 ./test/test_suite.sh 2>&1 | tr '\r' '\n' > test.log
 cat test.log
 cat test.log | grep -q 'success rate: 100%'
